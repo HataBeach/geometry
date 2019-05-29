faaf1325baae91d7dc330ba7e184c24677edc92f
Krakazya8lya <kulshin2007@outlook.com>
2019-05-29 23:49:09 +0700

    reformat code in clang format

--- src/Pars.cpp
+++ src/Pars.cpp
@@ -13,11 +13,11 @@ string partStr(string a)
 
 int partStr2(string a)
 {
-    if(i == 1)
+    if (i == 1)
         return 1;
-    if(i == 2)
+    if (i == 2)
         return 4;
-    if(i == 3)
+    if (i == 3)
         return 5;
     return 0;
 }
