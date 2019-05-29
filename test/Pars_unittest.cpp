438cf94b288b4f546f37e7ab7b5ff80b0f071b89
Krakazya8lya <kulshin2007@outlook.com>
2019-05-30 00:27:55 +0700

    reload bug fix

--- test/Pars_unittest.cpp
+++ test/Pars_unittest.cpp
@@ -1,11 +1,10 @@
 #include "../src/Pars.hpp"
 #include "gtest/gtest.h"
 #include <limits.h>
-
+int i = 0;
 namespace {
-TEST(partStrTest, Circle)
+    TEST(partStrTest, Circle)
 {
-    int i = 0;
     i = 0;
     ASSERT_EQ("circle", partStr("circle(1 4, 5)"));
     i = 0;
@@ -14,25 +13,23 @@ TEST(partStrTest, Circle)
 }
 TEST(partStrTest, NotCircle)
 {
-int i = 0;
-    i = 0;
-    EXPECT_NE("circle", partStr("dfsgs(1 4, 5)"));
-    i = 0;
-    EXPECT_NE("circle", partStr("Circle(6 3, 2)"));
-    i = 0;
-    EXPECT_NE("circle", partStr(""));
-    i = 0;
+i = 0;
+EXPECT_NE("circle", partStr("dfsgs(1 4, 5)"));
+i = 0;
+EXPECT_NE("circle", partStr("Circle(6 3, 2)"));
+i = 0;
+EXPECT_NE("circle", partStr(""));
+i = 0;
 }
 
 TEST(partStr2Test, IsNumber)
 {
-int i = 0;
-    i = 0;
-    partStr("circle(1 4, 5)");
-    i++;
-    EXPECT_EQ(1, partStr2("circle(1 4, 5)"));
-    EXPECT_EQ(4, partStr2("circle(1 4, 5)"));
-    EXPECT_EQ(5, partStr2("circle(1 4, 5)"));
-    i = 0;
+i = 0;
+partStr("circle(1 4, 5)");
+i++;
+EXPECT_EQ(1, partStr2("circle(1 4, 5)"));
+EXPECT_EQ(4, partStr2("circle(1 4, 5)"));
+EXPECT_EQ(5, partStr2("circle(1 4, 5)"));
+i = 0;
 }
 }
